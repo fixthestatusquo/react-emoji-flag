525626d5bb312002541b77d8be99f75bf4eeac0c
=Xavier <github@tttp.eu>
2024-06-06 22:40:10 +0200

    now works fine

--- src/index.js
+++ src/index.js
@@ -1,59 +1,49 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
+//import PropTypes from "prop-types";
 const fontUrl = "https://country-flag.proca.app/font/TwemojiCountryFlags.woff2";
 const fontName = "countryFlags";
 const className = "country-flag";
 let hasEffectRun = false;
 
-
 const nativeFlag = () => {
   const userAgent = window.navigator.userAgent;
   return userAgent.indexOf("Win") === -1;
-}
+};
 
-
-export function useCountryFlag (options)  {
-  
-  React.useEffect(() => {
+export function useCountryFlag(options) {
+  const cn = options?.className || className;
+  useEffect(() => {
     if (hasEffectRun) {
       // Run the effect logic only once
       return;
     }
- 
-    const cn = options?.className || className;   
+
     hasEffectRun = true;
     const css = `.${cn} {font-family: "${fontName}"}`;
 
     const loadFont = async () => {
-      const customFont = new FontFace(
-        fontName,
-        "url(" + fontUrl + ")",
-        {
-          unicodeRange:
-            "U+1F1E6-1F1FF, U+1F3F4, U+E0062-E0063, U+E0065, U+E0067, U+E006C, U+E006E, U+E0073-E0074, U+E0077, U+E007F",
-        }
-      );
-
+      const customFont = new FontFace(fontName, "url(" + fontUrl + ")", {
+        unicodeRange:
+          "U+1F1E6-1F1FF, U+1F3F4, U+E0062-E0063, U+E0065, U+E0067, U+E006C, U+E006E, U+E0073-E0074, U+E0077, U+E007F",
+      });
 
       try {
         await customFont.load();
         document.fonts.add(customFont);
-      } catch  {
+      } catch {
         console.log("can't load font");
       }
     };
 
-
-    if (nativeFlag()) 
-      return;
+    if (nativeFlag()) return;
 
     loadFont();
     const style = document.createElement("style");
     style.textContent = css;
     style.id = "react-emoji-flag";
     document.head.appendChild(style);
-  }, []);
-
-};
+  }, [cn]);
+}
 
 export const flag = (isoCode) => {
   const offset = 127397;
@@ -61,17 +51,25 @@ export const flag = (isoCode) => {
   return isoCode
     .toUpperCase()
     .replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + offset));
-// U+1F6A9
+  // U+1F6A9
 };
 
-const CountryFlag = (props) => {
-  const cn = props.className || className;
-  useCountryFlag({className:cn}); // load the font and create style if needed
+export const CountryFlag = (props) => {
+  const cn = props?.className || className;
+  useCountryFlag({ className: cn }); // load the font and create style if needed
   return (
     <span className={cn} title={props.title || props.countryCode}>
       {flag(props.countryCode)}
     </span>
   );
 };
+
+/*
+CountryFlag.propTypes = {
+  title: PropTypes.node,
+  countryCode: PropTypes.node,
+  className: PropTypes.node,
+};
+*/
 
 export default CountryFlag;
